7f78f69e61a59b5fead081abb236160531e2a785
ramiro-luengo <ramirobmds@gmail.com>
2022-07-07 14:00:07 -0300

    add test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const sinon = require('sinon');
 
 const app = require('../src');
 
@@ -8,11 +7,22 @@ chai.use(chaiHttp);
 chai.should();
 describe("Localidades", () => {
     describe("GET /api/localidades", () => {
-        it("fail to get localidades without auth token", (done) => {
+        it("without auth token", (done) => {
             chai.request(app)
                 .get('/api/localidades')
                 .end((err, res) => {
                     res.should.have.status(401);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+        it("with valid auth token", (done) => {
+            const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUGVkcm8iLCJpZCI6IjYyYzZmYjExNDUyYjE0ZmJmMDhmZGZjYSIsImlhdCI6MTY1NzIxMjk0Nn0.h15VvzimwKnuvl7UO7uCbAWALrIvgzLWyUER02UcVs8"
+            chai.request(app)
+                .get('/api/localidades')
+                .set('auth-token', validToken)
+                .end((err, res) => {
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
                     done();
                 });
