e18a8f35a1b79a5c9bf36d49ebeece11a6ff68da
ramiro-luengo <ramirobmds@gmail.com>
2022-07-07 13:42:40 -0300

    refactor logging mechanism

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,28 +1,36 @@
 const winston = require('winston');
 
+const logger = winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/all.log' }),
+    ],
+});
+
+const prettyJsonFormat = winston.format.printf(info => {
+    if (info.message.constructor === Object) {
+        info.message = JSON.stringify(info.message, null, 2)
+    }
+    return `${info.level}: ${info.message}`
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.prettyPrint(),
+            winston.format.splat(),
+            winston.format.simple(),
+            prettyJsonFormat,
+        )
+    }));
+}
+
 const getLogger = (filename) => {
     const relativePath = filename.split("\\src\\")[1]
 
-    const logger = winston.createLogger({
-        format: winston.format.json(),
-        defaultMeta: { module: relativePath },
-        transports: [
-            new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
-            new winston.transports.File({ filename: 'logs/all.log' }),
-        ],
-    });
-
-    if (process.env.NODE_ENV !== 'production') {
-        logger.add(new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.splat(),
-                // winston.format.simple(),
-            )
-        }));
-    }
-
-    return logger
+    return logger.child({ moduleName: relativePath });
 }
 
 module.exports = getLogger
